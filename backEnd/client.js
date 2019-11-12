b2acba04da07ef1131c5a952ae5b103a31fa80ca
Andy Shin <andyshin2@gmail.com>
2019-11-12 14:17:10 -0500

    lobby

--- backEnd/client.js
+++ backEnd/client.js
@@ -1,8 +1,150 @@
 var socket = io();
+
+var playerName = '';
+var gameCode = '';
+
 socket.on('message', function(data) {
   console.log(data);
 });
-document.getElementById("roomCreate").addEventListener("click", function(){
-  socket.emit('roomCreate',"");
-  console.log('roomCreate');
+
+socket.on('redirectLobbyUser',function(){
+  console.log('redirectLobbyUser');
+  redirectLobbyUFunc();
 });
+socket.on('redirectLobbyHost',function(){
+  console.log('redirectLobbyHFunc');
+  redirectLobbyHFunc();
+});
+socket.on('gameCode',function(data){
+  gameCode = data;
+});
+
+// room join button listener
+document.getElementById("roomJoin").addEventListener("click",roomJoinFunc);
+
+// room create button listener
+document.getElementById("roomCreate").addEventListener("click",roomCreateFunc);
+
+// room join function
+function roomJoinFunc(){
+  document.getElementById("mainDiv").innerHTML = 
+  `
+<h5 class="card-title text-center">Broker</h5>
+              <form class="form-signin">
+                <div class="form-group">
+                  <input type="email" id="playerName" class="form-control" placeholder="Player Name" required autofocus>
+                </div>
+                <div class="form-group">
+                  <input type="email" id="roomCode" class="form-control" placeholder="Room Code" required autofocus>
+                </div>
+                <button name="room" id="roomInit" type="button" value="join" class="btn btn-lg btn-primary btn-block text-uppercase">Join Room</button>
+              </form>
+  `;
+  // Create Lobby Listener
+  document.getElementById("roomInit").addEventListener("click",function(){
+    gameCode = document.getElementById('roomCode').value;
+    playerName = document.getElementById('playerName').value;
+    userJoinRoomFunc({'gameCode':gameCode,'playerName':playerName});
+  });
+}
+
+// room create function
+function roomCreateFunc(){
+  document.getElementById("mainDiv").innerHTML = 
+  `
+   <h5 class="card-title text-center">Broker</h5>
+              <form class="form-signin">
+
+                <div class="form-group">
+                  <input type="email" id="playerName" class="form-control" placeholder="Player Name" required autofocus>
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlSelect1">Game Type</label>
+                  <select class="form-control" id="exampleFormControlSelect1">
+                    <option>BlackJack</option>
+                    <option>Texas Hold'em</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlSelect1">Stack Size</label>
+                  <input type="email" id="playerName" class="form-control" placeholder="Enter number." required autofocus>
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlSelect1">Room Limit</label>
+                  <select class="form-control" id="exampleFormControlSelect1">
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                  </select>
+                </div>
+                <button name="room" id="roomInit" type="button" value="join" class="btn btn-lg btn-primary btn-block text-uppercase">Create Room</button>
+              </form>
+  `
+  ;
+  // Create Lobby Listener
+  document.getElementById("roomInit").addEventListener("click",hostStartRoomFunc);
+};
+
+// Create Lobby Function
+function hostStartRoomFunc(){
+  console.log("roomInit");
+  socket.emit("roomInit","");
+};
+
+// Join Lobby Function
+function userJoinRoomFunc(player){
+  console.log("roomJoin");
+  socket.emit("roomJoin",player);
+};
+
+// user redirect to lobby function
+function redirectLobbyUFunc(){
+  document.getElementById("mainDiv").innerHTML =
+  `
+   <h5 class="card-title text-center">Room #\number </h5>
+              <!-- Add the divs for current members -->
+              <div class="alert alert-secondary" role="alert">
+                Joseph Han
+              </div>
+              <!-- the yellow one to mark the host-->
+              <div class="alert alert-warning" role="alert">
+                Andy Shin
+              </div>
+              <div class="alert alert-secondary" role="alert">
+                Jin Kim
+              </div>
+
+
+              <!-- use something similar below to disable the settings menu and start game button for non host players -->
+              <!-- document.getElementById("myP").style.visibility = "hidden"; -->
+  `;
+};
+// host user redirect to lobby function
+function redirectLobbyHFunc(){
+  document.getElementById("mainDiv").innerHTML =
+  `
+  <h5 class="card-title text-center">Room #\number </h5>
+              <!-- Add the divs for current members -->
+              <div class="alert alert-secondary" role="alert">
+                Joseph Han
+              </div>
+              <!-- the yellow one to mark the host-->
+              <div class="alert alert-warning" role="alert">
+                Andy Shin
+              </div>
+              <div class="alert alert-secondary" role="alert">
+                Jin Kim
+              </div>
+
+
+              <!-- use something similar below to disable the settings menu and start game button for non host players -->
+              <!-- document.getElementById("myP").style.visibility = "hidden"; -->
+              <button name="room" type="submit" value="start" class="btn btn-lg btn-primary btn-block text-uppercase">Start Game</button>
+  `;
+};
--- server.js
+++ server.js
@@ -2,29 +2,53 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var socketIO = require('socket.io');var app = express();
+var socketIO = require('socket.io');
+var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
 app.set('port', 5000);
 app.use('/backEnd', express.static(__dirname + '/backEnd'));
 // Routing
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, '/frontEnd/landing_page.html'));
+  response.sendFile(path.join(__dirname, '/frontEnd/index.html'));
 });
 // Starts the server.
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
 
-var games = [];
+var games = {};
 var gameC = 0;
 
 // Add the WebSocket handlers
 io.on('connection', function(socket) {
-  var game = "";
-  socket.on('roomCreate', function(){
+  socket.on('roomInit', function(){
     gameC += 1;
-    game = gameC.toString().padStart(5, "0");
+    var game = gameC.toString().padStart(5, "0");
     console.log(game);
+    socket.emit('gameCode',game);
+    socket.emit('redirectLobbyHost',"");
+    games[game] = ['STANDINHOSTUSERNAME'];
+    socket.join(game);
   });
+  socket.on('roomJoin', function(player){
+    var gameCode = player['gameCode'];
+    var playerName = player['playerName'];
+    socket.emit('message',games);
+    socket.emit('message',gameCode);
+    socket.emit('message',playerName);
+    if(gameCode in games){
+      socket.emit('message','game exists');
+      socket.join(gameCode);
+      games[gameCode].push(playerName);
+      socket.emit('redirectLobbyUser',"");
+    }
+    else{
+      socket.emit('message','game does not exist');
+    }
+  });
+});
+
+io.on('message', function(data){
+  console.log(data);
 });
